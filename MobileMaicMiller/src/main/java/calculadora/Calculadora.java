312b5adee7402b849764c6ebf5f8a98d7971f038
Maic Miller <maicmiller42@gmail.com>
2021-09-21 09:32:31 -0300

    chore():add Appium dependency.
    Desired capabilities configuration.

---
+++ MobileMaicMiller/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+package calculadora;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class Calculadora {
+	
+	public static void main(String[] args) throws MalformedURLException {
+		
+	    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    desiredCapabilities.setCapability("platformName", "Android");
+	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+	    desiredCapabilities.setCapability("automationName", "uiautomator2");
+	    desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+	    desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+
+	    AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+		
+	}
+
+}
